a3021d2246e6c04efda3152796c49971c797ef22
Patrick Joyce <pjaudiomv@gmail.com>
2025-04-29 22:49:35 -0400

    relative base

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 import { svelteTesting } from '@testing-library/svelte/vite';
 
 export default defineConfig({
-  base: '/semantic/',
+  base: './',
   plugins: [
     svelte(),
     svelteTesting()
